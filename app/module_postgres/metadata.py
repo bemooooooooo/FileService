3b3c2a77556721d49cd7b1f5960f93d7f08bfe53
Name <you@example.com>
2024-11-02 01:34:10 +0300

    add good doc in metadata

--- app/module_postgres/metadata.py
+++ app/module_postgres/metadata.py
@@ -1,32 +1,26 @@
 from datetime import datetime
-
 from fastapi import UploadFile, File
 
 from app.module_postgres import uuid_generator
 
-"""_summary_
-    Metadata of the file, when it's upload to the filesystem
-    
-    _fields_
-    name - Name of the file
-    size - Size of the file
-    owner - Owner of the file and folder
-    create - Date when file was create
-    uuid - Name of the file in system
-"""
-
-
 class Metadata:
+    """## Metadata of the file, when it's upload to the filesystem.
+        
+        ### Fields:
+        - name - Name of the file.
+        - size - Size of the file.
+        - owner - Owner of the file and folder.
+        - create - Date when file was create.
+        - uuid - Name of the file in system.
+    """
     name: str
     size: int
     owner_id: int
     create: datetime
     uuid: int
 
-    """_summary_
-        Initialization of the metadata class with validation
-    """
     def __init__(self, owner_id: int, file: UploadFile = File(...)) -> None:
+        """## Initialization of the metadata class with validation"""
         if len(file.filename) > 30:
             raise ValueError("Имя файла не должно превышать 30 символов.")
         self.name = file.filename
